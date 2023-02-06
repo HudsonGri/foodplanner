b5e3b8a567dead7f81078cdf9fdf608f3cb673ab
Michael Tang <michael.tang@ufl.edu>
2023-02-06 16:32:15 -0500

    initial database setup

--- backend/main.go
+++ backend/main.go
@@ -4,8 +4,11 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
+	"01-Login/controllers"
+	"01-Login/models"
 	"01-Login/platform/authenticator"
 	"01-Login/platform/router"
 )
@@ -26,4 +29,17 @@ func main() {
 	if err := http.ListenAndServe("0.0.0.0:3000", rtr); err != nil {
 		log.Fatalf("There was an error with the http server: %v", err)
 	}
+
+	// connected database - commented out the above authentication code to test this
+	r := gin.Default()
+
+	models.ConnectDatabase()
+
+	r.GET("/users", controllers.FindUsers)
+	r.POST("/users", controllers.CreateUser)
+	r.GET("/users/:id", controllers.FindUser)
+	r.PATCH("/users/:id", controllers.UpdateUser)
+	r.DELETE("/users/:id", controllers.DeleteUser)
+
+	r.Run()
 }
