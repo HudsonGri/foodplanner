d66592aa061ff1c8342714bf7b7aa58637bfc3cf
Hudson <ardroneheart@gmail.com>
2023-02-07 23:39:30 -0500

    Adjusted file structure to account for flask api

--- backend/go/models/setup.go
+++ backend/go/models/setup.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open("sqlite3", "database.sqlite3")
+	database, err := gorm.Open("sqlite3", "../database.sqlite3")
 
 	if err != nil {
 		panic("Failed to connect to database!")
