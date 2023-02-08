182b1cd69facf27b79c919ef11d85b1c1d643ba4
Hudson <ardroneheart@gmail.com>
2023-02-07 23:37:54 -0500

    Adjusted database and models to account for simple user preferences

--- backend/main.go
+++ backend/main.go
@@ -1,36 +1,14 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 
 	"01-Login/controllers"
 	"01-Login/models"
-	"01-Login/platform/authenticator"
-	"01-Login/platform/router"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Failed to load the env vars: %v", err)
-	}
 
-	auth, err := authenticator.New()
-	if err != nil {
-		log.Fatalf("Failed to initialize the authenticator: %v", err)
-	}
-
-	rtr := router.New(auth)
-
-	log.Print("Server listening on http://localhost:3000/")
-	if err := http.ListenAndServe("0.0.0.0:3000", rtr); err != nil {
-		log.Fatalf("There was an error with the http server: %v", err)
-	}
-
-	// connected database - commented out the above authentication code to test this
 	r := gin.Default()
 
 	models.ConnectDatabase()
