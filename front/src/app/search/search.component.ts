2cfc72143b3389fde8581b136c7a61f3b003b41c
Derek Mo <derek.mo7203@gmail.com>
2023-02-27 18:01:49 -0500

    cards to generate based array in typescript file

--- front/src/app/search/search.component.ts
+++ front/src/app/search/search.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Card {
+  title: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-
+  cards: Card[] = [
+    {
+      title: 'Card 1',
+      description: 'This is the first card.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the second card.'
+    },
+    {
+      title: 'Card 3',
+      description: 'This is the third card.'
+    }
+  ];
 }
