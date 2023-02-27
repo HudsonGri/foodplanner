0aa5044622f7396e4c8235ed046e696cd7a773ce
Hudson <ardroneheart@gmail.com>
2023-02-27 17:59:54 -0500

    Added example to test calling apis

--- front/src/app/home/card/card.component.ts
+++ front/src/app/home/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-card',
@@ -6,4 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  
+  userIP:any;
+  
+  constructor(public auth: AuthService, private http: HttpClient) {}
+
+
+  ngOnInit() {      
+    // Simple POST request with a JSON body and response type <any>
+    this.http.get<any>('http://ip-api.com/json').subscribe(data => {
+        this.userIP = data.query;
+        
+    })}
 }
