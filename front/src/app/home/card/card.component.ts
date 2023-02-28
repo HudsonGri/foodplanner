36cb9ec827b0b385fda562e930747da69339ee6e
Hudson <ardroneheart@gmail.com>
2023-02-27 19:16:22 -0500

    Sending post request with user email

--- front/src/app/home/card/card.component.ts
+++ front/src/app/home/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { HttpClient } from '@angular/common/http';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-card',
@@ -8,16 +9,31 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-  
-  userIP:any;
-  
-  constructor(public auth: AuthService, private http: HttpClient) {}
+
+  userIP: any;
+  a_user: any;
+
+  constructor(public auth: AuthService, private http: HttpClient) { }
 
 
-  ngOnInit() {      
+  ngOnInit() {
     // Simple POST request with a JSON body and response type <any>
     this.http.get<any>('http://ip-api.com/json').subscribe(data => {
-        this.userIP = data.query;
-        
-    })}
+      this.userIP = data.query;
+
+      this.auth.user$.subscribe(user => {
+        this.a_user = user;
+        console.log(this.a_user)
+
+        this.http.post<any>('https://reqres.in/api/users', JSON.stringify({
+          "name": this.a_user.email,
+          "job": "leader"
+      }),).subscribe(data => {
+          console.log(data)
+
+        })
+      });
+
+    })
+  }
 }
