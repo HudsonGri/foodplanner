f613fc7b69ba674fc7047cbe7cc459b4315fbf98
Hudson <ardroneheart@gmail.com>
2023-03-27 17:24:59 -0400

    Added recipe calculation logic

--- backend/database.sqlite3.sql
+++ backend/database.sqlite3.sql
@@ -5,8 +5,10 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"email"	TEXT,
 	"skill_level"	INTEGER,
 	"cuisine_choices"	TEXT,
+	"recipes"	TEXT,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
-INSERT INTO "users" VALUES (1,'Michael T','michael.t@gmail.com',2,'[''mexican'']');
-INSERT INTO "users" VALUES (2,'Hudson G','hudsongriffith@gmail.com',2,'[''italian'']');
+INSERT INTO "users" VALUES (1,'Michael T','michael.t@gmail.com',2,'mexican',NULL);
+INSERT INTO "users" VALUES (2,'Hudson G','hudsongriffith@gmail.com',2,'italian','{}');
+INSERT INTO "users" VALUES (4,'Derek Mo','derek.mo7203@gmail.com',2,'gluten,mediterranean',NULL);
 COMMIT;
