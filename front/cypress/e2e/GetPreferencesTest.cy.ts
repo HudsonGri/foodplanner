fa29bf64c632571463b99aa180f71b932c6b9e2e
Derek Mo <derek.mo7203@gmail.com>
2023-04-19 21:13:14 -0400

    edited getpreferences test

--- front/cypress/e2e/GetPreferencesTest.cy.ts
+++ front/cypress/e2e/GetPreferencesTest.cy.ts
@@ -1,8 +1,24 @@
-// describe('gettingPreferences', () => {
-//     it('return the correct values', () => {
-//         cy.request({method: 'GET', url: 'http://localhost:8080/users/1'}).then(response => {
-//             expect(response.status).to.eq(200)
-//             expect(response.body).to.have.property('cuisine_choices')
-//          })
-//     })  
-// })
+describe('PreferencesComponent', () => {
+    beforeEach(() => {
+      cy.visit('/preferences');
+    });
+  
+    it('should get user preferences with GET request', () => {
+      cy.intercept('GET', 'http://localhost:8080/users/4').as('getUserPreferences');
+  
+      cy.get('#preferencesBtn').click();
+      cy.wait('@getUserPreferences').its('response.statusCode').should('eq', 200);
+  
+      cy.get('@getUserPreferences').then((interception) => {
+        const responseData = interception.response.body.data;
+  
+        expect(responseData.id).to.exist;
+        expect(responseData.cuisine_choices).to.exist;
+        expect(responseData.cuisine_choices.diets).to.exist;
+        expect(responseData.cuisine_choices.allergies).to.exist;
+        expect(responseData.cuisine_choices.cuisines).to.exist;
+  
+        // Add more expectations as needed for the data returned from the GET request
+      });
+    });
+  });
